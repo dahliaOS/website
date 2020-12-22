aec3b3c92d2bbfdda50c42feda9733b22d75a350
looskie <50378828+Looskie@users.noreply.github.com>
2020-12-22 09:34:10 -0500

    Create dashboardRouter.js

---
+++ routes/dashboardRouter.js
@@ -0,0 +1,26 @@
+const routes = require('express').Router();
+
+// Database
+const Users = require('../models/Users');
+
+routes.get('/', (req, res) => {
+    if (req.user) {
+        Users.findOne({ username: req.user.username }, (err, user) => {
+            res.render('dashboard.ejs', { user })
+        })
+    }
+})
+
+// Account api
+
+routes.post('/changePfP', (req, res) => {
+    const username = req.user.username;
+    const githubUser = req.body.pfp;
+    const icon = `https://github.com/${githubUser}.png`
+    Users.findOneAndUpdate({ username }, { $set: { icon } }, (err, user) => {
+        if (err) return console.log(err);
+        res.redirect('/logout');
+    })
+})
+
+module.exports = routes;
