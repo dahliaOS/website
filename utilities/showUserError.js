b2c1815c72aa539f79e9333dc0ab5ad09e4d12b9
looskie <50378828+Looskie@users.noreply.github.com>
2021-01-15 14:10:12 -0500

    Comments

--- utilities/showUserError.js
+++ utilities/showUserError.js
@@ -1,3 +1,8 @@
+// Import this module as throwError
+// So like this:
+// const throwError = require('../utilities/showUserError');
+// And then used like this throwError(res, 'STRING', 'STRING')
+
 module.exports = (res, directTo, message) => {
   console.log('direct To:', directTo, 'because:', message);
   res.render(directTo, { error: message });
