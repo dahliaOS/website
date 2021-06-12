50ed45c7dd2d65437142d7cd81dcf155e66d6b74
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-12 10:57:50 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,18 @@
 import Document, { Head, Html, NextScript, Main } from "next/document";
 
 const Page = () => {
-  return <Html lang='en'></Html>;
+  return (
+    <Html lang='en'>
+      <Head>
+        <meta charSet='UTF-8' />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 };
 
 export default class extends Document {
