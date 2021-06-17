f624ad51c889bc01bf74f778871f2523c1c94fcf
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-17 12:30:33 -0400

    Fix: Fixed the SSRendering of the CSS

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,42 @@
 import Document, { Head, Html, NextScript, Main } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core";
+import { theme } from "../util/theme";
+import React from "react";
 
-const Page = () => {
-  return (
-    <Html lang='en'>
-      <Head>
-        <meta charSet='UTF-8' />
-      </Head>
+export default class Page extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <meta charSet='UTF-8' />
+          <meta name='theme-color' content={theme.palette.primary.main} />
+        </Head>
 
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
-
-export default class extends Document {
-  render(): JSX.Element {
-    return <Page />;
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
+
+Page.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
