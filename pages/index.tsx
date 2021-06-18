47d40833b277edad575973efa8e0469ec7b1392d
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-18 14:23:23 -0400

    Feat: Hero almost finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from "../components/navbar";
 import { Footer } from "../components/footer";
-import { Box, makeStyles, Theme, Typography } from "@material-ui/core";
+import { Box, Button, makeStyles, Theme, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) => ({
   home: {
@@ -11,13 +11,16 @@ const useStyles = makeStyles((theme: Theme) => ({
     marginTop: "-60px",
   },
   homeHeader: {
-    fontSize: "2.5em",
+    fontSize: "3.5em",
+    fontWeight: theme.typography.fontWeightMedium,
+    fontFamily: "Sulphur Point, sans-serif",
     display: "block",
-    marginBottom: 10,
+    marginBottom: 5,
   },
   homeParagraph: {
     display: "block",
     fontSize: "1.8em",
+    fontWeight: theme.typography.fontWeightLight,
     maxWidth: "50ch",
     marginTop: 0,
   },
@@ -30,6 +33,17 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   heroImg: {
     width: "100%",
+  },
+  btnContainer: {
+    display: "inline-block",
+  },
+  gradientBtn: {
+    padding: "8px 12px",
+    borderRadius: 5,
+    "&:first-child": {
+      background: `linear-gradient(153deg, ${theme.palette.primary.dark} 0%, ${theme.palette.primary.light} 100%)`,
+      marginRight: 15,
+    },
   },
 }));
 
@@ -45,6 +59,10 @@ export default function Home() {
             dahliaOS is a modern, secure, lightweight and responsive operating
             system, combining the best of GNU/Linux and Fuchsia OS.
           </p>
+          <div className={classes.btnContainer}>
+            <Button className={classes.gradientBtn}>Download</Button>
+            <Button className={classes.gradientBtn}>Learn more</Button>
+          </div>
         </div>
         <div className={classes.homeHero}>
           <img className={classes.heroImg} src='/img/mockups/hero.png' />
