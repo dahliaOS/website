43248e1ad3b2f168cfb2e614621e99ec88b28e96
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-17 22:51:40 -0400

    Feat: Start of landing apge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,54 @@
 import { Navbar } from "../components/navbar";
 import { Footer } from "../components/footer";
-import { Box } from "@material-ui/core";
+import { Box, makeStyles, Theme, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  home: {
+    display: "flex",
+    minHeight: "100vh",
+    maxWidth: "100vw",
+    alignItems: "center",
+    marginTop: "-60px",
+  },
+  homeHeader: {
+    fontSize: "2.5em",
+    display: "block",
+    marginBottom: 10,
+  },
+  homeParagraph: {
+    display: "block",
+    fontSize: "1.8em",
+    maxWidth: "50ch",
+    marginTop: 0,
+  },
+  typography: {
+    flex: "50%",
+    paddingLeft: 50,
+  },
+  homeHero: {
+    flex: "50%",
+  },
+  heroImg: {
+    width: "100%",
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
   return (
     <div>
       <Navbar />
-      <Box>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
+      <Box className={classes.home}>
+        <div className={classes.typography}>
+          <h1 className={classes.homeHeader}>something new</h1>
+          <p className={classes.homeParagraph}>
+            dahliaOS is a modern, secure, lightweight and responsive operating
+            system, combining the best of GNU/Linux and Fuchsia OS.
+          </p>
+        </div>
+        <div className={classes.homeHero}>
+          <img className={classes.heroImg} src='/img/mockups/hero.png' />
+        </div>
       </Box>
       <Footer />
     </div>
