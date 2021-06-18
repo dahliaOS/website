cdf49c5b96d074af4495770a2358f4af975d7c06
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-18 14:23:12 -0400

    Feat: Fonts + Icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,18 @@ export default class Page extends Document {
         <Head>
           <meta charSet='UTF-8' />
           <meta name='theme-color' content={theme.palette.primary.main} />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Sulphur+Point:wght@300;400&amp;display=swap'
+            rel='stylesheet'
+          />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+          />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/icon?family=Material+Icons'
+          />
         </Head>
 
         <body>
