324ff293a34b3b4da789dd998d7c7be5374aaee0
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-19 22:55:05 -0400

    Feat: Donate page

---
+++ pages/donate.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "../components/footer";
+import { Navbar } from "../components/navbar";
+import { Box } from "@material-ui/core";
+
+export default function download() {
+  return (
+    <div>
+      <Navbar />
+      <Box>Donate</Box>
+      <Footer />
+    </div>
+  );
+}
