896edaae58c445ec719ebf9750168b4ef25d9d62
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-19 23:28:36 -0400

    Chore: License comment in pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,14 +8,34 @@ export default class Page extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <head
+            dangerouslySetInnerHTML={{
+              __html: `<!-- 
+              Copyright 2021 The dahliaOS Authors\n
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+-->`,
+            }}
+          />
+
           <meta charSet='UTF-8' />
-          <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
           <link rel='canonical' href='https://dahliaos.io' />
           <meta name='theme-color' content={theme.palette.primary.main} />
           <meta property='og:image' content='https://imgur.com/PNGGkDP.png' />
           <meta property='og:site_name' content='dahliaOS' />
           <meta name='apple-mobile-web-app-capable' content='yes' />
-          <link rel="icon" type="image/png" href="/img/favicon.png" />
+          <link rel='icon' type='image/png' href='/img/favicon.png' />
           <meta
             name='apple-mobile-web-app-status-bar-style'
             content='#ff3d00'
