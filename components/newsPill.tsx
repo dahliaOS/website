f1df378d12d4e89c873c2faa8e0eab436c86a8df
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-23 20:33:54 -0400

    Fix: No more yelling thanks

--- components/newsPill.tsx
+++ components/newsPill.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Link, makeStyles, Theme } from "@material-ui/core";
 import UpdateIcon from "@material-ui/icons/Update";
 
@@ -6,13 +7,13 @@ const useStyles = makeStyles((theme: Theme) => ({
     color: "unset",
   },
   featured: {
-    verticalAlign: "middle",
-    display: "inline-block",
-    background: theme.palette.secondary.light,
-    padding: "8px 15px",
-    borderRadius: 8,
-    boxShadow: theme.shadows[3],
-    transition: `${theme.transitions.easing.easeInOut} 0.229s`,
+    "verticalAlign": "middle",
+    "display": "inline-block",
+    "background": theme.palette.secondary.light,
+    "padding": "8px 15px",
+    "borderRadius": 8,
+    "boxShadow": theme.shadows[3],
+    "transition": `${theme.transitions.easing.easeInOut} 0.229s`,
     "&:hover": {
       boxShadow: theme.shadows[6],
     },
@@ -28,10 +29,9 @@ export const NewsPill = () => {
   const classes = useStyles();
 
   return (
-    <Link href='/github' className={classes.removeLinkStyle}>
+    <Link href="/github" className={classes.removeLinkStyle}>
       <span className={classes.featured}>
-        <UpdateIcon className={classes.verticalCenter} /> New version
-        build-201215
+        <UpdateIcon className={classes.verticalCenter} /> New version build-201215
       </span>
     </Link>
   );
