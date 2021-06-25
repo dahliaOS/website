69b29e15804b6f6eead67efd288e14fca316dca9
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-25 02:19:13 -0400

    Refactor: Refactor donate.tsx and cleanup

--- pages/donate.tsx
+++ pages/donate.tsx
@@ -1,28 +1,11 @@
 import React from "react";
 
 import { Footer, Navbar } from "../components/index";
-import { Box, makeStyles, Theme, Typography, Button } from "@material-ui/core";
-
-const useStyles = makeStyles((theme: Theme) => ({
-  header: {
-    fontSize: "2.2em",
-    fontWeight: theme.typography.fontWeightMedium,
-    textAlign: "center",
-  },
-  container: {
-    width: "90%",
-    maxWidth: 500,
-    margin: "0 auto",
-    textAlign: "left",
-    fontSize: "1.2em",
-  },
-  btn: {
-    background: `linear-gradient(153deg, ${theme.palette.primary.dark} 0%, ${theme.palette.primary.light} 100%)`,
-  },
-}));
+import { Box, Typography, Button } from "@material-ui/core";
+import { donatePage } from "../styles/index";
 
 export default function download(): JSX.Element {
-  const classes = useStyles();
+  const classes = donatePage();
 
   return (
     <div>
