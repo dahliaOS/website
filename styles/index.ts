66284ba332796ab108c7e47bb36c740d0726756a
looskie <50378828+Looskie@users.noreply.github.com>
2021-06-25 02:17:15 -0400

    Feat: add things to index.ts for styles

--- styles/index.ts
+++ styles/index.ts
@@ -2,3 +2,7 @@
 more than one thing from components, you're welcome */
 
 export * from "./indexPage";
+export * from "./cocPage";
+export * from "./donatePage";
+export * from "./downloadPage";
+export * from "./fuchsiaPage";
