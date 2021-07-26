491e3aa3dd1420fa60ce76b02a04ec088cd8de52
Cody Miller <hello@looskie.com>
2021-07-25 22:01:19 -0400

    Feat: Fetcher for all hooks

---
+++ utils/fetcher.ts
@@ -0,0 +1,13 @@
+export const fetcher = async (url: string) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    const error: any = new Error("An error occurred whilst fetching data!");
+
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
