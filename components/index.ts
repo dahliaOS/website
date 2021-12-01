e7e40c13e8cd0cbb240f4131d15493710f705509
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-12-01 10:17:50 -0500

    follow react standards

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,8 @@
 /* This file exists just in case you want cleaner imports and need to import
 more than one thing from components, you're welcome */
 
-export * from "./Downloads";
-export * from "./Footers";
-export * from "./Navbars";
-export * from "./NewsPills";
-export * from "./Intros";
+export * from "./Download";
+export * from "./Footer";
+export * from "./Navbar";
+export * from "./NewsPill";
+export * from "./Intro";
