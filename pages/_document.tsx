ea43c5d00e144778359e1e40d7b27c6e4e8003ec
Cody Miller <50378828+Looskie@users.noreply.github.com>
2022-02-03 19:08:48 -0500

    comment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,18 @@ import { Theme } from "./utils/Theme";
 const Page = () => (
   <Html lang="en">
     <Head>
+      {/*
+      Copyright 2021 The dahliaOS Authors\n
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+      */}
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <link rel="canonical" href="https://dahliaos.io" />
