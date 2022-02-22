7037e72af744dcb77d855f3f974e97e6304f2dcc
Cody Miller <50378828+Looskie@users.noreply.github.com>
2022-02-21 22:48:42 -0500

    github files

---
+++ pages/coc.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import coc from "../c"
+
+const Wrapper = styled.div`
+
+`
+
+const Coc = () => {
+  return (
+    <>
+      <Navbar />
+      <Wrapper>
+        <pre>
+
+        </pre>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Coc;
