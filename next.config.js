6c7caf9e850c493e377669b43e6699da2bfaee6f
Cody Miller <50378828+Looskie@users.noreply.github.com>
2022-02-22 00:08:11 -0500

    coc page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
   async redirects() {
     return [
       {
--- pages/coc.tsx
+++ pages/coc.tsx
@@ -1,20 +1,37 @@
 import styled from "styled-components";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import coc from "../c"
+import coc from "../CONTRIBUTING.md";
+import { Theme } from "../utils/Theme";
 
 const Wrapper = styled.div`
+  width: 90%;
+  max-width: 700px;
+  text-align: left;
+  font-size: 1em;
+  margin: 130px auto;
+`;
 
-`
+const Header = styled.h1`
+  color: ${Theme.text.textColor};
+  font-weight: 500;
+  margin-bottom: 50px;
+`;
+
+const Pre = styled.pre`
+  white-space: pre-wrap;
+  color: ${Theme.text.textColor};
+`;
 
 const Coc = () => {
   return (
     <>
       <Navbar />
       <Wrapper>
-        <pre>
-
-        </pre>
+        <Header>Code of Conduct</Header>
+        <Pre>{coc}</Pre>
       </Wrapper>
+      <Footer />
     </>
   );
 };
