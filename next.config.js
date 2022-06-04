6fbc3a5b522cc01587576cb9ce8cba9820958ece
Marin Heđeš <hedesmarin@gmail.com>
2022-06-04 19:21:32 +0200

    cleanup: remove unusued import, file

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: config => {
     config.module.rules.push({
-      test: /\.md$/,
-      use: "raw-loader",
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
