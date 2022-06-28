c77455e69f831fdfcb4d16ed84ccfaafc939f37d
SincerelyFaust <hedesmarin@gmail.com>
2022-06-28 23:42:18 +0200

    babel ignores you

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
